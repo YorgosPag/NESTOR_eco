7e13f0af5d9f2fac64c26e746aeaee73d2f18dbd
Γιώργος Παγώνης <georgios.pagonis@gmail.com>
2025-07-11 16:29:52 +0000

    Add WASM config to Next.js build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+// next.config.js
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      config.experiments = { ...config.experiments, asyncWebAssembly: true };
+      config.module.rules.push({
+        test: /\.wasm$/,
+        type: "webassembly/async",
+      });
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
