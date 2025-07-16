dda57ffc734b0aa5760c59788744d80e34637098
Γιώργος Παγώνης <georgios.pagonis@gmail.com>
2025-07-16 08:29:53 +0000

    studio-7497881651:~/studio{main}$ npx jest --listTests /home/user/studio

--- src/__tests__/MyComponent.test.tsx
+++ src/__tests__/MyComponent.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import MyComponent from './MyComponent';
+import MyComponent from '@/components/MyComponent';
 
 test('εμφανίζει το κουμπί', () => {
   render(<MyComponent />);
