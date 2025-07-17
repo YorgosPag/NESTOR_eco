78d4de8761a77319bac92afd38762ecc6310f9eb
Γιώργος Παγώνης <georgios.pagonis@gmail.com>
2025-07-17 08:55:09 +0000

    studio-7497881651:~/studio{main}$  /usr/bin/env 'NODE_OPTIONS= --require

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,7 @@
+// jest.setup.js
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder as typeof global.TextDecoder;
 
 // Optional: configure or set up a testing framework before each test.
 // If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
