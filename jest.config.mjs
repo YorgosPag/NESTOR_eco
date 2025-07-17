1b2465ab97c2f6e689cd1552ff4211aa7571c6e3
Γιώργος Παγώνης <georgios.pagonis@gmail.com>
2025-07-17 08:31:39 +0000

    at Object.<anonymous> (src/components/dashboard/project-card.test.tsx:22

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,38 +1,22 @@
+// jest.config.mjs
+import nextJest from 'next/jest.js';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { readFileSync } from 'fs';
 
-import nextJest from 'next/jest.js'
- 
+const { compilerOptions } = JSON.parse(readFileSync('./tsconfig.json'));
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
- 
+});
+
 // Add any custom config to be passed to Jest
-/** @type {import('jest').Config} */
-const config = {
-  // Add more setup options before each test is run
+const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    // Handle module aliases (this will be important for things like "components/ui")
-    '^@/components/(.*)$': '<rootDir>/src/components/$1',
-    '^@/lib/(.*)$': '<rootDir>/src/lib/$1',
-    '^@/hooks/(.*)$': '<rootDir>/src/hooks/$1',
-    '^@/app/(.*)$': '<rootDir>/src/app/$1',
-  },
-  testPathIgnorePatterns: [
-    "<rootDir>/.next/",
-    "<rootDir>/node_modules/",
-  ],
-  transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
-  },
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$',
-  ],
-}
- 
+  preset: 'ts-jest',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
+};
+
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+export default createJestConfig(customJestConfig);
